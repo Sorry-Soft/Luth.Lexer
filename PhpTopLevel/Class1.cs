b7af103c3fab4176bb7b2f366178c6e8f449499f
Butterfingers89 <matthewsd03@hotmail.com>
2024-03-01 21:10:01 +0000

    fix: Assembly Factory late bind issue

---
+++ PhpTopLevel/Class1.cs
@@ -0,0 +1,62 @@
+﻿using Luth.IntergrationPack;
+using Luth.IntergrationPack.Interfaces;
+using System.Drawing;
+using System.Text.RegularExpressions;
+
+namespace PhpTopLevel
+{
+    public class Class1 : IIdentifier
+    {
+        public int Orderby => 0;
+
+        public string TokenType => "phpStart";
+
+        public Color Color => Color.AliceBlue;
+
+        public bool IsInError(Token? previousToken)
+        {
+            return false;
+        }
+
+        public bool MatchesRule(string input)
+        {
+             return input.StartsWith("<?php")  && input.EndsWith("?>");
+        }
+    }
+
+    public class SpaceMutationStrategy : IPretokenMutation
+    {
+        public string OldValue => " ";
+
+        public string NewValue => "<space>";
+
+        public string Mutate(string input)
+        {
+            return input.Replace(OldValue, NewValue);
+        }
+    }
+
+    public class PhpStartTagFinderStrategy : IPretokenMutation
+    {
+        public string OldValue => "<?";
+
+        public string NewValue => " <?";
+
+        public string Mutate(string input)
+        {
+            return input.Replace(OldValue, NewValue);
+        }
+    }
+
+    public class PhpEndTagFinderStrategy : IPretokenMutation
+    {
+        public string OldValue => "?>";
+
+        public string NewValue => "?> ";
+
+        public string Mutate(string input)
+        {
+            return input.Replace(OldValue, NewValue);
+        }
+    }
+}
---
+++ Runner/Program.cs
@@ -0,0 +1,11 @@
+﻿using Luth;
+using Luth.IntergrationPack;
+
+LexerBuilder LexerBuilder = new LexerBuilder(); 
+Lexer l =LexerBuilder
+    .ConfigureLanguage("PhpTopLevelLanguagePack")
+    .Build();
+
+List<Token> tokens = l.Tokenise("<?php echo(\"Hello, World!\") ;?> ").ToList();
+
+tokens.ForEach(t => Console.WriteLine(t.Value));
